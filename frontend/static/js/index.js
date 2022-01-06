bf13c1cb92ed6dda32763d14863af63085d42a58
dwanerich <dwanerich@gmail.com>
2022-01-06 11:37:14 -0500

    created asyn router method & map thru routes to test path match

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,27 @@
-console.log('this is my index.js file')
+// console.log('this is my index.js file')
+
+const router = async () => {
+    const routes = [
+        {path: "/", view: () => (console.log('viewing dashboard'))},
+        { path: "/drums", view: () => (console.log('viewing drums')) },
+        { path: "/branding", view: () => (console.log('viewing branding')) },
+
+    ];
+
+    // testing each route for match
+
+    const routeMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    let match = routeMatches.find(routeMatches => routeMatches.isMatch);
+
+    console.log(match, routeMatches, match);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router()
+})
